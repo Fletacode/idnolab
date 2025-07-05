b24fe98a3390501281b5b689d34344cdcac25276
Ubuntu <ubuntu@ip-172-31-33-222.ap-northeast-2.compute.internal>
2025-07-05 08:35:13 +0000

    update item

--- main2.py
+++ main2.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
             processed_count = 0
             
             for index, item in items_from_row3.items():
-                if index <= 170:
+                if index <= 547:
                     continue
                 # result = get_industry_data_with_gemini(item)
                 # data = parse_industry_data_with_gemini(result)
